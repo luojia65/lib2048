e150eb8a1674d5aa6ae2816cefcd1e719022cb9f
luojia65 <me@luojia.cc>
2018-07-18 21:31:41 +0800

    Rename

--- src/rand2048.rs
+++ src/rand2048.rs
@@ -3,21 +3,21 @@ use Board;
 use TilePos;
 
 // 90% is 1u8(shown as 2), 10% is 2u8(shown as 4).
-pub(crate) fn next_tile_value() -> u8 {
+pub(crate) fn tile_value() -> u8 {
     if thread_rng().gen_bool(0.9) { 1 } else { 2 }
 }
 
-pub(crate) fn next_random_pos(b: &Board) -> Option<TilePos> {
+pub(crate) fn pos(b: &Board) -> Option<TilePos> {
     unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
-    use rand2048::next_tile_value;
+    use rand2048::tile_value;
     #[test]
     fn generate_tile_value() {
         for _ in 0..1000 {
-            let a = next_tile_value();
+            let a = tile_value();
             assert!(a == 1 || a == 2);
         }
     }
