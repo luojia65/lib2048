56a36cceba28097327a61509050a747688583c65
luojia65 <me@luojia.cc>
2018-07-18 21:30:47 +0800

    Random pos for generating. TODO here.

--- src/rand2048.rs
+++ src/rand2048.rs
@@ -1,9 +1,15 @@
 use rand::prelude::*;
+use Board;
+use TilePos;
+
 // 90% is 1u8(shown as 2), 10% is 2u8(shown as 4).
 pub(crate) fn next_tile_value() -> u8 {
     if thread_rng().gen_bool(0.9) { 1 } else { 2 }
 }
 
+pub(crate) fn next_random_pos(b: &Board) -> Option<TilePos> {
+    unimplemented!()
+}
 
 #[cfg(test)]
 mod tests {
